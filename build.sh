52cb9416921caa34d2e08845ba82359397b795db
anbuchelva <anbuchelva@gmail.com>
2018-05-31 03:15:16 +0530

    modify build script

--- build.sh
+++ build.sh
@@ -3,18 +3,10 @@ npm install
 git config --global user.name anbuchelva
 git config --global user.email anbuchelva@gmail.com
 sed -i'' "s~git@github.com:anbuchelva/blog.git~https://${GH_TOKEN}:x-oauth-basic@github.com/anbuchelva/blog.git~" _config.yml
-cp -rf themes/blog_tranquilpeak/* themes/tranquilpeak
-cd themes/tranquilpeak
-npm install
-npm install grunt-cli -g
-npm install bower -g
-bower install
-grunt buildProd
-cd ..
 hexo clean
 hexo algolia
-#pwd #to identify present working directory
-cp ../source/admin/config.yml ../public/admin/config.yml
+pwd #to identify present working directory
+hexo generate
+cp source/admin/config.yml public/admin/config.yml
 # cp ../source/admin/netlify.css ../public/admin/netlify.css
-# hexo generate
 hexo deploy --silent
---
+++ theme.sh
@@ -0,0 +1,20 @@
+if [ ! -d "themes/tranquilpeak" ]; then
+	# cd ../
+	# echo installing zip
+	# apt-get update
+	# apt-get install zip unzip
+	echo downloading theme
+	wget https://github.com/LouisBarranqueiro/hexo-theme-tranquilpeak/releases/download/v2.0.0/hexo-theme-tranquilpeak-built-for-production-2.0.0.zip --quiet
+	echo unzipping
+	unzip -qo hexo-theme-tranquilpeak-built-for-production-2.0.0.zip -d themes/
+	echo rename theme folder
+	mv themes/hexo-theme-tranquilpeak-built-for-production-2.0.0 themes/tranquilpeak
+	echo remove unnecessary files
+	rm -rf themes/__MACOSX
+	rm -rf hexo-theme-*.zip
+	echo copy blog_tranquilpeak to tranquilpeak
+	cp -rf themes/blog_tranquilpeak/* themes/tranquilpeak
+	cd themes/tranquilpeak
+	npm install
+	cd ../../
+fi
