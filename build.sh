f930de4be949a02c8dd3ddc5152a20737d726935
Netlify <netlify@mailinator.com>
2018-05-30 12:15:40 +0530

    update build script

--- build.sh
+++ build.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 npm install
-git config --global user.name "Netlify"
-git config --global user.email "netlify@mailinator.com"
-cd ../
+git config --global user.name anbuchelva
+git config --global user.email anbuchelva@gmail.com
 sed -i'' "s~git@github.com:anbuchelva/blog.git~https://${GH_TOKEN}:x-oauth-basic@github.com/anbuchelva/blog.git~" _config.yml
 cp -rf themes/blog_tranquilpeak/* themes/tranquilpeak
 cd themes/tranquilpeak
 npm install
-npm run lint
-npm run prod
+npm install grunt-cli -g
+npm install bower -g
+bower install
+grunt buildProd
 cd ..
 hexo clean
 hexo algolia
