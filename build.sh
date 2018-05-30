5e325ab469cb718f782e3dfcc769b3be2b4c0676
anbuchelva <anbuchelva@gmail.com>
2018-05-30 13:09:34 +0530

    enable build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+npm install
+git config --global user.name anbuchelva
+git config --global user.email anbuchelva@gmail.com
+sed -i'' "s~git@github.com:anbuchelva/hyperion.git~https://${GH_TOKEN}:x-oauth-basic@github.com/anbuchelva/hyperion.git~" _config.yml
+cp -rf themes/pure_hyperion/* themes/pure/
+hexo clean
+cp source/admin/config.yml public/admin/config.yml
+hexo deploy
