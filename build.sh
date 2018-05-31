a3353112c76dbb5a81fffa1c5f6c5b092a7cecd8
anbuchelva <anbuchelva@gmail.com>
2018-05-31 12:03:37 +0530

    build with different ci
    Signed-off-by: anbuchelva <anbuchelva@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 npm install
-git config --global user.name Netlify
-git config --global user.email anbuchelva@gmail.com
 sed -i'' "s~git@github.com:anbuchelva/blog.git~https://${GH_TOKEN}:x-oauth-basic@github.com/anbuchelva/blog.git~" _config.yml
 hexo clean
 hexo algolia
@@ -9,3 +7,15 @@ pwd #to identify present working directory
 hexo generate
 cp source/admin/config.yml public/admin/config.yml
 # cp ../source/admin/netlify.css ../public/admin/netlify.css
+if [ ${ci}="Netlify" ]; then
+	git config --global user.name 'anbuchelva via Netlify'
+	git config --global user.email netlify@netlify.com
+elif [ ${ci}="Travis-CI" ]; then
+	git config --global user.name 'anbuchelva via Travis-CI'
+	git config --global user.email travis-ci@Travis-CI.org
+else
+	git config --global user.name anbuchelva
+	git config --global user.email anbuchelva@gmail.com
+fi
+echo ${ci}
+hexo deploy --silent
