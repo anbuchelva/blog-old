990588b269525d5a8fbd1d19a1051cab4b42e1a0
anbuchelva <anbuchelva@gmail.com>
2018-06-18 01:19:06 +0530

    fix travis build

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-npm install
-git config --global user.name anbuchelva
-git config --global user.email anbuchelva@gmail.com
-sed -i'' "s~git@github.com:anbuchelva/blog.git~https://${GH_TOKEN}:x-oauth-basic@github.com/anbuchelva/blog.git~" _config.yml
-npm run generate
-cp source/admin/config.yml public/admin/config.yml
-npm run deploy
